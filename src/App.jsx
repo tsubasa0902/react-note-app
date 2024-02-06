a5c76923933bdb198825e7cb0ba4093ede086ba8
tsubasa <tsubasa.macbook0902@gmail.com>
2024-02-07 06:20:25 +0900

    初期コミット

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Sidebar from './compornents/Sidebar'
+import Main from './compornents/Main'
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+      <Main />
+    </div>
+  )
+}
+
+export default App
